ec42fc6ebeddb0f64deb9d44e40b0d7c2c7e90d1
Omar Mendoza <omaryahir@gmail.com>
2013-02-26 12:57:08 -0600

    The software send a message in a Notification Center every hour

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 notifyhour
 ==========
 
-Notify something every hour in the notification center of the mac
+Notify something every hour in the notification center in Mac OSX.
+
+## Execution
+
+$ sudo python notifyhour.py
+
+It requires:
+- https://github.com/setem/pync
+
+
+I accept suggestions ... thanks !
--- notifyhour.py
+++ notifyhour.py
@@ -1,3 +1,22 @@
 # This software send every hour some notification in Mac OSX
+# It requires: https://github.com/setem/pync
 
-print "En Desarrollo"
+from pync import Notifier
+import datetime 
+
+print "Working process ...   [Press Ctrl+C for exit]"
+print "Powered by Python, Mac OSX & https://github.com/setem/pync"
+
+enviemensaje = False
+now = datetime.datetime.now()
+while now.hour<=18:
+    now = datetime.datetime.now()
+    if now.minute == 0:
+        if not enviemensaje:
+            mensaje = "Dios te ama, descansa un momento "+str(now.hour)+":"+str(now.minute)        
+            Notifier.notify(str(mensaje))
+            enviemensaje=True
+    if now.minute == 1:
+        enviemensaje=False
+
+Notifier.notify("Sea como sea Omar hiciste tu mejor esfuerzo, vamonos..!")
