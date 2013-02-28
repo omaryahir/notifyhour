c460cd7247c5875404129c533cf92871c5d47c63
Omar Mendoza <omaryahir@gmail.com>
2013-02-28 13:15:53 -0600

    Now the Mac speaks with the message

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 notifyhour
 ==========
 
-Notify something every hour in the notification center in Mac OSX.
+Notify something every hour in the notification center in Mac OSX. Also the Mac speak with the message.
+
+I hope was useful for you...
+
+NOTE: Message are in spanish, this software I develop for me, because I need to rest my eyes every hour.
 
 ## Execution
 
--- notifyhour.py
+++ notifyhour.py
@@ -1,6 +1,7 @@
 # This software send every hour some notification in Mac OSX
 # It requires: https://github.com/setem/pync
 
+from os import system
 from pync import Notifier
 import datetime 
 
@@ -8,15 +9,24 @@ print "Working process ...   [Press Ctrl+C for exit]"
 print "Powered by Python, Mac OSX & https://github.com/setem/pync"
 
 enviemensaje = False
+activaalarma = False
 now = datetime.datetime.now()
 while now.hour<=18:
     now = datetime.datetime.now()
     if now.minute == 0:
         if not enviemensaje:
+        	system('say Omar, descansa un momento tus ojos, recuerda Dios te ama.')
             mensaje = "Dios te ama, descansa un momento "+str(now.hour)+":"+str(now.minute)        
             Notifier.notify(str(mensaje))
             enviemensaje=True
+            activaalarma=True
+    
     if now.minute == 1:
-        enviemensaje=False
+        enviemensaje=False        
+
+    if now.minute == 3:
+    	if activaalarma:    		
+    		system('say Listo, ahora puedes continuar trabajando, debes tener fe, todo estará bien.')
+    		activaalarma=False
 
 Notifier.notify("Sea como sea Omar hiciste tu mejor esfuerzo, vamonos..!")
